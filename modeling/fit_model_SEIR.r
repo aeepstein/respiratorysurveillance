7047fa9ab2fd0657394b41f325c9224e95cdaa42
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-04-01 10:08:49 -0700

    full mcmc to attack rate estimate pipeline

--- modeling/fit_model_SEIR.r
+++ modeling/fit_model_SEIR.r
@@ -3,10 +3,17 @@
 library(odin2)
 library(dust2)
 library(monty)
+library(parallel)
+
+args <- commandArgs(trailingOnly = TRUE)
+simulation_file <- args[1] # e.g. "sim_200"
+n_simulations <- as.numeric(args[2]) # e.g. 500
+MCMC_length <- as.numeric(args[3]) # recommended value 2e6
 
 # download data
-data <- read.csv("sim_200.csv")
+data <- read.csv(paste0(simulation_file,".csv"))
 colnames(data)[1] <- "time"
+sim_length <- nrow(data)%/%n_simulations
 
 # Stochastic discrete SEIR model
 seir <- odin({
@@ -41,15 +48,6 @@ seir <- odin({
   active_cases ~ Poisson(I)
 }, quiet = TRUE)
 
-pars <- list(beta = 3.7, gamma = 2, alpha=1, E0 = 7, R0 = 1500, sigma=0.45, phi=3)
-# sys <- dust_system_create(seir(), pars, dt = 0.25,deterministic = TRUE)
-# dust_system_set_state_initial(sys)
-# time <- 0:100/4
-# y <- dust_system_simulate(sys, time)
-# print(dust_unpack_state(sys,y)$R[101]-dust_unpack_state(sys,y)$R[1])
-# prevalence <- dust_unpack_state(sys,y)$prevalence
-# matplot(time, prevalence, type = "l", col = "grey", lty = 1, lwd = 1, xlab = "Time", ylab = "Prevalence", main = "Prevalence trajectories")
-
 prior <- monty_dsl({
   beta ~ Exponential(mean=0.15)
   gamma ~ Exponential(mean=0.1)
@@ -60,20 +58,8 @@ prior <- monty_dsl({
 })
 
 seir_packer <- monty_packer(c("beta", "gamma", "E0", "R0", "sigma", "phi"), fixed = list(alpha=1))
-print(seir_packer$pack(pars))
 
-# each 36 weeks is a different group
-one_run <- data[(36*5+1):(36*6),]
-unfilter <- dust_unfilter_create(seir(), 0, one_run)
-likelihood <- dust_likelihood_monty(unfilter, seir_packer)
-
-posterior <- prior + likelihood
-
-# sampler with step of 0.01 for beta and gamma, and 1 for E0 and R0
-# vcv <- matrix(c(0.004, 0, 0, 0,
-#                 0, 0.005, 0, 0,
-#                 0, 0, 0.1, 0,
-#                 0, 0, 0, 0.1), 4, 4)
+# step sizes chosen to give acceptance rate of around 0.23
 vcv <- matrix(c(0.0008, 0, 0, 0, 0.0004, 0,
                 0, 0.0008, 0, 0, 0, 0,
                 0, 0, 0.1, 0, 0, 0,
@@ -82,40 +68,10 @@ vcv <- matrix(c(0.0008, 0, 0, 0, 0.0004, 0,
                 0, 0, 0, 0, 0, 0.004), 6, 6)
 sampler <- monty_sampler_random_walk(vcv)
 
-# n_samples = 2e6
-# samples <- monty_sample(posterior, sampler, n_samples, initial = seir_packer$pack(pars))
-# plot(samples$density, type="l")
-# print(samples$pars[((n_samples-1)*nrow(vcv)+1):(n_samples*nrow(vcv))])
-
-
-# acceptance_rate <- 1 - sum(apply(samples$pars,1,duplicated))/(n_samples*nrow(vcv))
-# print(paste("Acceptance rate: ", acceptance_rate))
-# # plot(t(drop(samples$pars)))
-
-# # save results
-# write.csv(samples$pars, "samples_SEIR.csv")
-
-# # calculate medians and 95% credible intervals of the parameters
-# pars <- samples$pars
-# med <- apply(pars, 1, median)
-# ci <- apply(pars, 1, function(x) quantile(x, c(0.025, 0.975)))
-# print(med)
-# print(ci)
-
-# parallel version
-library(parallel)
-n_cores <- detectCores()
-cl <- makeCluster(n_cores)
-clusterExport(cl, c("data","seir", "unfilter", "prior", "seir_packer", "sampler", "pars"))
-clusterEvalQ(cl, library(odin2))
-clusterEvalQ(cl, library(dust2))
-clusterEvalQ(cl, library(monty))
-clusterEvalQ(cl, library(parallel))
-clusterEvalQ(cl, library(stats))
-clusterEvalQ(cl, library(base))
+### run MCMCs in parallel for all simulations
 # define function to run the model
 run_model <- function(i, n_samples) {
-  one_run <- data[((i-1)*36+1):(i*36),]
+  one_run <- data[((i-1)*sim_length+1):(i*sim_length),]
   unfilter <- dust_unfilter_create(seir(), 0, one_run)
   pars <- list(beta = 3.7, gamma = 2, alpha=1, E0 = 7, R0 = 1500, sigma=0.45, phi=3)
   likelihood <- dust_likelihood_monty(unfilter, seir_packer)
@@ -133,32 +89,29 @@ run_model <- function(i, n_samples) {
     y <- dust_system_simulate(sys, time)
     attack_rates[par_n] <- dust_unpack_state(sys,y)$R[151]-dust_unpack_state(sys,y)$R[1]
   }
-  # write to median and CI to same file
-  write.csv(rbind(med, ci), paste0("MCMC_runs_SEIR/med_ci", i, ".csv"))
-  write.csv(samples$pars, paste0("MCMC_runs_SEIR/pars", i, ".csv"))
-  write.csv(attack_rates, paste0("MCMC_runs_SEIR/attack_rates", i, ".csv"))
-  # write.csv(samples$density, paste0("MCMC_runs_SEIR/density", i, ".csv"))
+  attack_med <- median(attack_rates)
+  attack_ci <- quantile(attack_rates, c(0.025, 0.975))
+  # write to median and CI to the same file
+  med_ci <- rbind(i,med, ci)
+  attack_med_ci <- t(c(i,attack_med, attack_ci))
+  write.table(med_ci, file = paste0("parameter_med_ci_",simulation_file,".csv"), append = TRUE, sep = ",", col.names = FALSE, row.names = FALSE)
+  write.table(attack_med_ci, file = paste0("attack_med_ci",simulation_file,".csv"), append = TRUE, sep = ",", col.names = FALSE, row.names = FALSE)
 }
+
+# set MCMC length
 model_partial <- function(i) {
-  run_model(i, 2e6)
+  run_model(i, MCMC_length)
 }
-mclapply(1:3, model_partial, mc.cores = n_cores)
+# write header to the file
+header <- t(c("n_simulation", "beta_med", "beta_lower", "beta_upper", "gamma_med", "gamma_lower", "gamma_upper", "E0_med", "E0_lower", "E0_upper", "R0_med", "R0_lower", "R0_upper", "sigma_med", "sigma_lower", "sigma_upper", "phi_med", "phi_lower", "phi_upper"))
+write.table(header, file = paste0("parameter_med_ci_",simulation_file,".csv"), sep = ",", col.names = FALSE, row.names = FALSE)
+header_attack <- t(c("n_simulation", "median", "lower", "upper"))
+write.table(header_attack, file = paste0("attack_med_ci",simulation_file,".csv"), sep = ",", col.names = FALSE, row.names = FALSE)
+# run the model
+mclapply(1:n_simulations, model_partial, mc.cores = detectCores())
 
-# # histogram of gamma and beta distribution, excluding first 2000 samples
-# hist(samples$pars[1,4001:10000,], breaks=50, col="blue", xlab="gamma")
-# hist(samples$pars[2,4001:10000,], breaks=50, col="red")
-
-# # multipanel plot of histograms of all four parameters
-# par(mfrow=c(2,2))
-# for (i in 1:4) {
-#   hist(samples$pars[i,4001:10000,], breaks=50, col="blue", xlab=names(pars)[i])
-# }
-
-# # Plot a bunch of runs of the model against the data
-# sys <- dust_system_create(seir(), pars, n_particles = 20, dt = 0.25)
-# dust_system_set_state_initial(sys)
-# x <- dust_likelihood_run(unfilter, pars,save_trajectories = TRUE)
-# h <- dust_likelihood_last_trajectories(unfilter)
-# prevalence <- dust_unpack_state(sys,h)$prevalence
-# matplot(t(prevalence), type = "l", col = "grey", lty = 1, lwd = 1, xlab = "Time", ylab = "Prevalence", main = "Prevalence trajectories")
-# points(one_run$time, one_run$prev, col = "black", pch = 19, cex = 0.5)
+# calculate median and ci of attack rate medians
+attack_med_ci <- read.csv(paste0("attack_med_ci",simulation_file,".csv"), header = TRUE)
+attack_med <- attack_med_ci$median
+print(attack_med)
+print(quantile(attack_med, c(0.025, 0.5, 0.975)))
--- modeling/fit_model_seasonal.R
+++ modeling/fit_model_seasonal.R
@@ -6,7 +6,8 @@ library(monty)
 
 # download data
 data <- read.csv("sim_200.csv")
-one_run <- data[1:36,]
+i <- 100
+one_run <- data[(36*i+1):(36*(i+1)),]
 # rename "week" column to "time"
 colnames(one_run)[1] <- "time"
 
@@ -38,49 +39,63 @@ sir <- odin({
   active_cases ~ Poisson(I)
 }, quiet = TRUE)
 
-filter <- dust_filter_create(sir(), 0, one_run, n_particles = 1000)
+unfilter <- dust_unfilter_create(sir(), 0, one_run)
 
-pars <- list(beta = 4.24e-4, gamma = 1, I0 = 0, R0 = 1.5e3, sigma = 1, phi = 18)
+pars <- list(beta = 0.0001, gamma = 1, I0 = 0, R0 = 1.5e3, sigma = 1, phi = 18)
 
 prior <- monty_dsl({
-  beta ~ Exponential(mean=0.0007)
-  # R0 ~ Exponential(mean=1500)
+  beta ~ Exponential(mean=0.0001)
+  R0 ~ Exponential(mean=1500)
   # sigma ~ Exponential(mean=1)
   # phi ~ Exponential(mean=15)
 })
 
-sir_packer <- monty_packer(c("beta"), fixed = list(gamma=1, I0 = 0,sigma=1, phi=18, R0=1500))
+sir_packer <- monty_packer(c("beta","R0"), fixed = list(gamma=1, I0 = 0,sigma=1, phi=18))
 
-likelihood <- dust_likelihood_monty(filter, sir_packer)
+likelihood <- dust_likelihood_monty(unfilter, sir_packer)
 
 posterior <- prior + likelihood
 
-# sampler with step of 0.01 for beta and gamma, and 1 for I0 and R0
-acceptances <- c()
-step_sizes <- c(1,1e-1,1e-2,1e-3,1e-13,1e-14,1e-15,1e-16)
-for (step_size in step_sizes) {
-  vcv <- diag(c(step_size),1,1)
-  sampler <- monty_sampler_random_walk(vcv)
-  n_samples <- 1e4
-  samples <- monty_sample(posterior, sampler, n_samples, initial = sir_packer$pack(pars))
-  acceptance_rate <- 1 - sum(apply(samples$pars,1,duplicated))/n_samples
-  print(paste("Step size: ", step_size, "Acceptance rate: ", acceptance_rate))
-  acceptances <- c(acceptances, acceptance_rate)
-  # print(apply(samples$pars,1,mean))
-}
-# log x axis
-plot(step_sizes, acceptances, type="l", log="x", xlab="Step size", ylab="Acceptance rate")
+# # sampler with step of 0.01 for beta and gamma, and 1 for I0 and R0
+# acceptances <- c()
+# step_sizes <- c(1,1e-1,1e-2,1e-3,1e-13,1e-14,1e-15,1e-16)
+# for (step_size in step_sizes) {
+#   vcv <- diag(c(step_size),1,1)
+#   sampler <- monty_sampler_random_walk(vcv)
+#   n_samples <- 1e4
+#   samples <- monty_sample(posterior, sampler, n_samples, initial = sir_packer$pack(pars))
+#   acceptance_rate <- 1 - sum(apply(samples$pars,1,duplicated))/n_samples
+#   print(paste("Step size: ", step_size, "Acceptance rate: ", acceptance_rate))
+#   acceptances <- c(acceptances, acceptance_rate)
+#   # print(apply(samples$pars,1,mean))
+# }
+# # log x axis
+# plot(step_sizes, acceptances, type="l", log="x", xlab="Step size", ylab="Acceptance rate")
 # vcv <- diag(c(1e-11),1,1)
-# # vcv <- diag(c(1e-10,1e-7),2,2)
-# # vcv <- matrix(c(0.0005, 0.0003, 0, 0.0005,
-# #                 0.0003, 0.0003, 0.0003, 0,
-# #                 0, 0.0003, 0.01, 0,
-# #                 0.0005, 0, 0, 0.1), 4, 4)
-# sampler <- monty_sampler_random_walk(vcv)
+vcv <- diag(c(1e-8,7),2,2)
+# vcv <- matrix(c(0.0005, 0.0003, 0, 0.0005,
+#                 0.0003, 0.0003, 0.0003, 0,
+#                 0, 0.0003, 0.01, 0,
+#                 0.0005, 0, 0, 0.1), 4, 4)
+sampler <- monty_sampler_random_walk(vcv)
 
-# n_samples = 1e5
-# samples <- monty_sample(posterior, sampler, n_samples, initial = sir_packer$pack(pars))
-# plot(samples$density, type="l")
+n_samples = 1e6
+samples <- monty_sample(posterior, sampler, n_samples, initial = sir_packer$pack(pars))
+plot(samples$density, type="l")
+acceptance_rate <- 1 - sum(apply(samples$pars,1,duplicated))/(n_samples*nrow(vcv))
+print(paste("Acceptance rate: ", acceptance_rate))
+pars_samples <- samples$pars[,(3*n_samples%/%4):n_samples,1]
+print(apply(pars_samples,1,median))
+attack_rates <- numeric((n_samples%/%4))
+for (par_n in 1:((n_samples%/%4))) {
+  sys_pars <- list(beta = pars_samples[1,par_n], gamma = 1, I0 = 0, R0 = pars_samples[2,par_n], sigma = 1, phi = 18)
+  sys <- dust_system_create(sir(), sys_pars, dt = 0.25,deterministic = TRUE)
+  dust_system_set_state_initial(sys)
+  time <- 0:150/4
+  y <- dust_system_simulate(sys, time)
+  attack_rates[par_n] <- dust_unpack_state(sys,y)$R[151]-dust_unpack_state(sys,y)$R[1]
+}
+print(quantile(attack_rates, c(0.025,0.5,0.975)))
 # # save results
 # # write.csv(samples$pars, "samples_smaller.csv")
 # # calculate acceptance rate - i.e. 1 - the proportion of values in pars that are identical to the previous value
